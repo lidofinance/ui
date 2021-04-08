f9d8315bb713f6c24963cd51350ccd20a27f6956
kolyasapphire <a@ks.gg>
2021-04-08 13:15:02 +0300

    Formatting

--- packages/button/src/index.js
+++ packages/button/src/index.js
@@ -6,83 +6,83 @@ import PropTypes from 'prop-types'
 import shouldForwardProp from '@styled-system/should-forward-prop'
 
 const APPEARANCES = {
-    PRIMARY: 'primary',
+  PRIMARY: 'primary',
 }
 
 const StyledButton = styled('button').withConfig({
-    shouldForwardProp,
+  shouldForwardProp,
 })`
-    border: none;
-    outline: none;
-    white-space: nowrap;
-    overflow: hidden;
-    line-height: inherit;
-    background-color: inherit;
-    font-size: inherit;
-    color: inherit;
-    padding: inherit;
+  border: none;
+  outline: none;
+  white-space: nowrap;
+  overflow: hidden;
+  line-height: inherit;
+  background-color: inherit;
+  font-size: inherit;
+  color: inherit;
+  padding: inherit;
 
-    background-color: ${theme.colors.main};
-    font-size: 18px;
-    font-weight: 500;
-    line-height: 24px;
-    color: #ffffff;
-    padding: 16px 20px;
-    border-radius: 6px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  background-color: ${theme.colors.main};
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 24px;
+  color: #ffffff;
+  padding: 16px 20px;
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    :hover {
-        background-color: #009bf2;
-    }
+  :hover {
+    background-color: #009bf2;
+  }
 
-    :hover {
-        cursor: pointer;
-    }
+  :hover {
+    cursor: pointer;
+  }
 
-    ${(props) =>
-        props.isFullWidth &&
-        css`
-            width: 100%;
-        `}
+  ${(props) =>
+    props.isFullWidth &&
+    css`
+      width: 100%;
+    `}
 
-    ${(props) =>
-        props.isDisabled &&
-        css`
-            background-color: #96cbfa;
-            :hover {
-                background-color: #96cbfa;
-            }
-        `}
+  ${(props) =>
+    props.isDisabled &&
+    css`
+      background-color: #96cbfa;
+      :hover {
+        background-color: #96cbfa;
+      }
+    `}
 
     ${inject}
 `
 
 const Button = ({ children, isLoading, loadingText, ...rest }) => (
-    <StyledButton {...rest}>
-        {loadingText && isLoading ? loadingText : children}
-    </StyledButton>
+  <StyledButton {...rest}>
+    {loadingText && isLoading ? loadingText : children}
+  </StyledButton>
 )
 
 Button.propTypes = {
-    isLoading: PropTypes.bool.isRequired,
-    isDisabled: PropTypes.bool.isRequired,
-    loadingText: PropTypes.string,
-    isFullWidth: PropTypes.bool,
-    leftIcon: PropTypes.func,
-    rightIcon: PropTypes.func,
-    variant: PropTypes.oneOf(Object.values(APPEARANCES)),
+  isLoading: PropTypes.bool.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+  loadingText: PropTypes.string,
+  isFullWidth: PropTypes.bool,
+  leftIcon: PropTypes.func,
+  rightIcon: PropTypes.func,
+  variant: PropTypes.oneOf(Object.values(APPEARANCES)),
 }
 
 Button.defaultProps = {
-    isLoading: false,
-    isDisabled: false,
-    loadingText: null,
-    isFullWidth: false,
-    leftIcon: null,
-    rightIcon: null,
-    variant: APPEARANCES.PRIMARY,
+  isLoading: false,
+  isDisabled: false,
+  loadingText: null,
+  isFullWidth: false,
+  leftIcon: null,
+  rightIcon: null,
+  variant: APPEARANCES.PRIMARY,
 }
 
 export default Button
