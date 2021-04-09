f231077cf9560314e7432a9e07e11c9f444ffa20
kolyasapphire <a@ks.gg>
2021-04-09 20:51:29 +0300

    Fixing tests by including React

--- packages/button/src/Button.test.js
+++ packages/button/src/Button.test.js
@@ -1,12 +1,11 @@
+import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
 
 import Button from '.'
 
-describe('Button', () => {
-  test('example', () => {
-    const tree = renderer.create(<Button>Test</Button>).toJSON()
+it('renders correctly', () => {
+  const tree = renderer.create(<Button>Test</Button>).toJSON()
 
-    expect(tree).toMatchSnapshot()
-  })
+  expect(tree).toMatchSnapshot()
 })
