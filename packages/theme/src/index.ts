e52b28777060a454598e1318c86e4dc12631b6f5
mymphe <azatsdev@gmail.com>
2021-04-16 11:34:06 +0600

    theme: add secondary text color

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -11,6 +11,7 @@ const theme = {
     background: '#F4F6F8',
     error: '#FF564F',
     text: '#0C141D',
+    textSecondary: '#5D6B7B',
     success: '#61B75F',
     border: '#d1d8df',
     borderHover: '#b1b7bd',
