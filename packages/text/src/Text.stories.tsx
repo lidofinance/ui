cf823add8ba4b9299ae3d9e5fb05a7cc570d708f
mymphe <azatsdev@gmail.com>
2021-04-16 11:33:01 +0600

    fix: stories

--- packages/text/src/Text.stories.tsx
+++ packages/text/src/Text.stories.tsx
@@ -5,10 +5,9 @@ export default {
   title: 'Lido UI/Text',
 }
 
-export const normal = () => <Text>Normal 16px text</Text>
 
+export const basic = () => <Text>Normal text</Text>
 export const small = () => <Text size='sm'>12px text</Text>
-
+export const medium = () => <Text size='md'>16px text</Text>
 export const large = () => <Text size='lg'>18px text</Text>
-
 export const extraLarge = () => <Text size='xl'>20px text</Text>
