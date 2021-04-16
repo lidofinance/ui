37255d43c00b26056c9378cfc20f6fd6f52489fb
mymphe <azatsdev@gmail.com>
2021-04-16 14:46:05 +0600

    rename component

--- packages/link/src/Link.tsx
+++ packages/link/src/Link.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react'
+import React from 'react'
 import { LinkStyle } from './LinkStyles'
 
 type Props = React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export default function Input({ children, ...props }: Props) {
+export default function Link({ children, ...props }: Props) {
   return (
     <LinkStyle target='_blank' rel='noopener' {...props}>
       {children}
