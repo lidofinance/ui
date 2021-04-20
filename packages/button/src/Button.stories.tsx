1dbfd6a3cfbf3236f7afdf722c1f120e98e6e380
kolyasapphire <a@ks.gg>
2021-04-20 16:29:40 +0300

    Button rework

--- packages/button/src/Button.stories.tsx
+++ packages/button/src/Button.stories.tsx
@@ -1,16 +1,16 @@
 import Button from './Button'
 
 export default {
-    component: Button,
-    title: 'Lido UI/Button',
+  component: Button,
+  title: 'Lido UI/Button',
 }
 
 export const basic = () => <Button>Button</Button>
 
 export const loading = () => (
-    <Button isLoading loadingText='Loading...'>
-        Button
-    </Button>
+  <Button isLoading loadingText='Loading...'>
+    Button
+  </Button>
 )
 
 export const disabled = () => <Button isDisabled>Button</Button>
