ed7e8d2b236cae7871d11f8ca2d363c68055bba3
George Avsetsin <mail@sjors.ru>
2021-05-11 17:07:40 +0300

    fix: InlineLoader blinking in safari

--- packages/loaders/src/InlineLoaderStyles.tsx
+++ packages/loaders/src/InlineLoaderStyles.tsx
@@ -4,7 +4,7 @@ import { InlineLoaderColors } from './types'
 
 export const translation = keyframes`
   100% {
-    transform: translateX(calc(2 / 3 * 100%));
+    background-position: 0 0;
   }
 `
 
@@ -19,26 +19,18 @@ export const InlineLoaderStyle = styled.div<{ $color: InlineLoaderColors }>`
   overflow: hidden;
   user-select: none;
   opacity: 0.05;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: -200%;
-    transform: translateX(0%);
-    animation: ${translation} 2s infinite;
-    background-image: linear-gradient(
-      90deg,
-      var(--loader-color) 0,
-      var(--loader-color) 33.33%,
-      transparent 44.44%,
-      transparent 55.55%,
-      var(--loader-color) 66.66%,
-      var(--loader-color) 100%
-    );
-  }
+  animation: ${translation} 2s infinite;
+  background-size: 300% 100%;
+  background-position: 100% 0;
+  background-image: linear-gradient(
+    90deg,
+    var(--loader-color) 0,
+    var(--loader-color) 33.33%,
+    transparent 44.44%,
+    transparent 55.55%,
+    var(--loader-color) 66.66%,
+    var(--loader-color) 100%
+  );
 
   &:after {
     content: '';
