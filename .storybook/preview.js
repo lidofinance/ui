c2cfdedbeb2e659da76792e75cb068e2fafc1389
George Avsetsin <mail@sjors.ru>
2021-05-11 15:57:57 +0300

    feature: accordion component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import { themes } from '@storybook/theming'
 import { DocsContainer } from './components/DocContainer'
+import theme from '../packages/theme/dist/esm'
 
 export const parameters = {
   docs: {
@@ -9,4 +10,21 @@ export const parameters = {
     dark: { ...themes.dark, appBg: 'rgb(42, 42, 42)' },
     light: { ...themes.normal },
   },
+  backgrounds: {
+    values: [
+      {
+        name: 'control',
+        value: theme.colors.controlBg,
+      },
+      {
+        name: 'background',
+        value: theme.colors.background,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.text,
+      },
+    ],
+  },
+  controls: { hideNoControlsWarning: true },
 }
