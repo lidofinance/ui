bcece6742dac7567a1e865ebca76f65011240c48
George Avsetsin <mail@sjors.ru>
2021-05-26 20:45:15 +0300

    feat: multi-semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+      },
+    ],
+    '@semantic-release/npm',
+  ],
+}
