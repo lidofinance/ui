8bc46f86f8918627ce4e150b653477892215df0d
mymphe <azatsdev@gmail.com>
2021-10-15 13:54:21 +0600

    fix(useSystemTheme): prevent from failing silently

--- packages/hooks/src/useSystemTheme.ts
+++ packages/hooks/src/useSystemTheme.ts
@@ -9,21 +9,17 @@ export const useSystemTheme = (): THEME => {
   const [systemTheme, setSystemTheme] = useState(THEME.light)
 
   useEffect(() => {
-    try {
-      const mql = window.matchMedia('(prefers-color-scheme: dark)')
+    const mql = window.matchMedia('(prefers-color-scheme: dark)')
 
-      const setThemeFromQuery = ({ matches }: { matches: boolean }) => {
-        setSystemTheme(matches ? THEME.dark : THEME.light)
-      }
+    const setThemeFromQuery = ({ matches }: { matches: boolean }) => {
+      setSystemTheme(matches ? THEME.dark : THEME.light)
+    }
 
-      mql.addEventListener('change', setThemeFromQuery)
-      setThemeFromQuery(mql)
+    mql.addEventListener('change', setThemeFromQuery)
+    setThemeFromQuery(mql)
 
-      return () => {
-        mql.removeEventListener('change', setThemeFromQuery)
-      }
-    } catch (error) {
-      return
+    return () => {
+      mql.removeEventListener('change', setThemeFromQuery)
     }
   }, [])
 
