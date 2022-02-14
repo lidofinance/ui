1d84825de94d5d39793ca410ee34bfdd65c35da5
kolyasapphire <a@ks.gg>
2022-02-14 13:23:15 +0300

    fix: fadeVisited should be optional for links

--- packages/link/src/types.ts
+++ packages/link/src/types.ts
@@ -1,4 +1,4 @@
 import { LidoComponentProps } from '@lidofinance/utils'
 export type { Theme } from '@lidofinance/theme'
 
-export type LinkProps = LidoComponentProps<'a'> & { fadeVisited: boolean }
+export type LinkProps = LidoComponentProps<'a'> & { fadeVisited?: boolean }
