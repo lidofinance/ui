06fc7e8285c991025759e51fc43137f7ac794df0
Alexander Khramov <alx.khramov@gmail.com>
2022-04-06 17:10:27 +0300

    feat(icons): add ArrowBack icon

--- packages/icons/src/index.tsx
+++ packages/icons/src/index.tsx
@@ -2,6 +2,31 @@
 
 import * as React from 'react'
 
+export const ArrowBack = React.forwardRef(function ArrowBack(
+  props: React.SVGProps<SVGSVGElement>,
+  svgRef?: React.Ref<SVGSVGElement>
+) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      ref={svgRef}
+      {...props}
+    >
+      <path
+        d='M20 12H4m6 6l-6-6 6-6'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth={2}
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+})
+
 export const ArrowBottom = React.forwardRef(function ArrowBottom(
   props: React.SVGProps<SVGSVGElement>,
   svgRef?: React.Ref<SVGSVGElement>
