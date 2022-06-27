3da92c084222a7891d355192915caa53189f3c9f
karina <karinamaulitova@gmail.com>
2022-06-27 13:48:15 +0200

    fix: tooltip padding

--- packages/tooltip/src/TooltipStyles.ts
+++ packages/tooltip/src/TooltipStyles.ts
@@ -2,15 +2,17 @@ import styled from 'styled-components'
 import { Popover } from '@lidofinance/popover'
 
 export const TooltipPopoverStyle = styled(Popover)`
-  padding: 12px;
-  background: ${({ theme }) => theme.colors.accent};
-  color: ${({ theme }) => theme.colors.accentContrast};
-  font-size: ${({ theme }) => theme.fontSizesMap.xxxs}px;
-  line-height: 1.8em;
-  font-weight: 400;
-  max-width: 256px;
-  border-radius: ${({ theme }) => theme.borderRadiusesMap.md}px;
-  box-shadow: ${({ theme }) =>
-    `${theme.boxShadows.sm} ${theme.colors.shadowLight}`};
-  pointer-events: none;
+  && {
+    padding: 12px;
+    background: ${({ theme }) => theme.colors.accent};
+    color: ${({ theme }) => theme.colors.accentContrast};
+    font-size: ${({ theme }) => theme.fontSizesMap.xxxs}px;
+    line-height: 1.8em;
+    font-weight: 400;
+    max-width: 256px;
+    border-radius: ${({ theme }) => theme.borderRadiusesMap.md}px;
+    box-shadow: ${({ theme }) =>
+      `${theme.boxShadows.sm} ${theme.colors.shadowLight}`};
+    pointer-events: none;
+  }
 `
