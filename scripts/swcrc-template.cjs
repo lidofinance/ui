515adb39891edec54c67e3aa51859b6e8b35bd6b
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-13 14:39:18 +0300

    feat: remove src in box pkg

--- scripts/swcrc-template.cjs
+++ scripts/swcrc-template.cjs
@@ -22,7 +22,7 @@ module.exports = {
       '@lidofinance/accordion': ['packages/accordion/index.ts'],
       '@lidofinance/address': ['packages/address/index.ts'],
       '@lidofinance/block': ['packages/block/index.ts'],
-      '@lidofinance/box': ['packages/box/src/index.ts'],
+      '@lidofinance/box': ['packages/box/index.ts'],
       '@lidofinance/button': ['packages/button/src/index.ts'],
       '@lidofinance/checkbox': ['packages/checkbox/src/index.ts'],
       '@lidofinance/chip': ['packages/chip/src/index.ts'],
