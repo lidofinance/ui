93459df562815e4bf2af5b334f6bf50b07c4a8de
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-14 16:14:11 +0300

    feat: better story

--- packages/content-theme/src/content-theme.stories.tsx
+++ packages/content-theme/src/content-theme.stories.tsx
@@ -1,4 +1,6 @@
 import { Story, Meta } from '@storybook/react'
+import { Text } from '@lidofinance/text'
+import { Block } from '@lidofinance/block'
 
 import { ContentTheme } from './content-theme'
 
@@ -9,8 +11,16 @@ export default {
 export const Basic: Story = () => (
   <>
     <ContentTheme
-      darkContent={'You see only dark content!'}
-      lightContent={'You see only light content!'}
+      darkContent={
+        <Block>
+          <Text>You see only dark content!</Text>
+        </Block>
+      }
+      lightContent={
+        <Block>
+          <Text>You see only light content!</Text>
+        </Block>
+      }
     />
   </>
 )
