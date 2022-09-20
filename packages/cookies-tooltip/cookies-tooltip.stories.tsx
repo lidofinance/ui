69fd7ee6c4fcf2d798feb185369784c6ed252104
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-20 12:41:30 +0300

    feat(cookies-tooltip): storybook

--- packages/cookies-tooltip/cookies-tooltip.stories.tsx
+++ packages/cookies-tooltip/cookies-tooltip.stories.tsx
@@ -1,9 +1,28 @@
 import { Story, Meta } from '@storybook/react'
 
+import { Text } from '@lidofinance/text'
 import { CookiesTooltip } from './cookies-tooltip'
 
 export default {
   title: 'CookiesTooltip/Basic',
 } as Meta
 
-export const Basic: Story = () => <CookiesTooltip />
+export const Basic: Story = () => (
+  <>
+    <Text>
+      CookiesTooltip component have a ability to detect a user choice in other
+      tabs and third level domain.
+    </Text>
+    <Text>
+      You can check above if you:
+      <br />
+      - duplicate this tab;
+      <br />
+      - allow or decline cookie;
+      <br />
+      - and check in another tab;
+      <br />- and click to work area (bug of storybook).
+    </Text>
+    <CookiesTooltip />
+  </>
+)
