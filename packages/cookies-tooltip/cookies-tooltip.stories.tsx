e7dc4187c0b7fe08e0f96ce0ce4d30af027b5948
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-20 10:57:52 +0300

    feat: cookies-tooltip component without cookie logic

---
+++ packages/cookies-tooltip/cookies-tooltip.stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+
+import { CookiesTooltip } from './cookies-tooltip'
+
+export default {
+  title: 'CookiesTooltip/Basic',
+} as Meta
+
+export const Basic: Story = () => <CookiesTooltip />
---
+++ packages/cookies-tooltip/cookies-tooltip.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useEffect, useState } from 'react'
+// import { AppCookies, COOKIES_ALLOWED_KEY } from 'utils'
+import { Cookie, CookieInverse } from '@lidofinance/icons'
+import { ContentTheme } from '@lidofinance/content-theme'
+
+import {
+  Wrap,
+  Box,
+  CookieIconWrap,
+  Text,
+  ButtonsWrap,
+  AllowButton,
+  DeclineButton,
+  Link,
+} from './styles'
+
+export const CookiesTooltip: FC = () => {
+  const [isVisible, setVisibility] = useState(false)
+
+  useEffect(() => {
+    // if (AppCookies.getCookie(COOKIES_ALLOWED_KEY) === null) {
+    //   setVisibility(true)
+    // }
+    setVisibility(true)
+  }, [])
+
+  if (!isVisible) return <></>
+
+  return (
+    <Wrap>
+      <Box>
+        <CookieIconWrap>
+          <ContentTheme
+            darkContent={<CookieInverse />}
+            lightContent={<Cookie />}
+          />
+        </CookieIconWrap>
+        <Text>
+          We use cookies to collect anonymous site visitation data
+          to&nbsp;improve the performance of&nbsp;our&nbsp;website.
+          For&nbsp;more info read our&nbsp;
+          <Link href='https://lido.fi/privacy-notice'>Privacy Notice</Link>
+        </Text>
+        <ButtonsWrap>
+          <AllowButton
+            onClick={() => {
+              // AppCookies.allowCookies()
+              setVisibility(false)
+            }}
+          >
+            Allow
+          </AllowButton>
+          <DeclineButton
+            onClick={() => {
+              // AppCookies.declineCookies()
+              setVisibility(false)
+            }}
+          >
+            Decline
+          </DeclineButton>
+        </ButtonsWrap>
+      </Box>
+    </Wrap>
+  )
+}
