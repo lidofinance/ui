12bbfa32f3fd9f9b0cbe6ff9a01dea4bd935d255
Anton Shalimov <a.shalimof@yandex.ru>
2022-09-20 11:46:32 +0300

    feat(utils): set/get cookie on client side

---
+++ packages/cookies-tooltip/utils.ts
@@ -0,0 +1,14 @@
+import { setCrossDomainCookieClientSide } from '@lidofinance/utils'
+import {
+  COOKIE_ALLOWED_KEY,
+  COOKIE_VALUE_NO,
+  COOKIE_VALUE_YES,
+} from './constants'
+
+export const allowCookies = (): void => {
+  setCrossDomainCookieClientSide(COOKIE_ALLOWED_KEY, COOKIE_VALUE_YES)
+}
+
+export const declineCookies = (): void => {
+  setCrossDomainCookieClientSide(COOKIE_ALLOWED_KEY, COOKIE_VALUE_NO)
+}
