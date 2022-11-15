bc5d5babc1cb63a4e11725cb3b8090d49b851490
Anton Shalimov <a.shalimof@yandex.ru>
2022-11-15 18:29:08 +0300

    fix: set cookie without Chrome warning

--- packages/utils/cookies-client-side.ts
+++ packages/utils/cookies-client-side.ts
@@ -1,3 +1,4 @@
+import { UAParser } from 'ua-parser-js'
 import { getTopLevelDomain } from './get-top-level-domain'
 
 export const setCrossDomainCookieClientSide = (
@@ -13,8 +14,16 @@ export const setCrossDomainCookieClientSide = (
   // 1. we want this cookie to be available on HTTP websites too.
   // 2. there is a bug on localhost which causes Chrome to ignore cookies set without Secure,
   // and Safari when cookies are set with Secure, so we're forcing cookie into both
-  document.cookie = cookie
-  document.cookie = `${cookie}Secure;`
+  const parser = new UAParser()
+  if (parser.getBrowser()?.name?.toLowerCase() === 'safari') {
+    if (window.location.protocol === 'https:') {
+      document.cookie = `${cookie}Secure;`
+    } else {
+      document.cookie = cookie
+    }
+  } else {
+    document.cookie = `${cookie}Secure;`
+  }
 }
 
 export const getCrossDomainCookieClientSide = (key: string): string | null => {
