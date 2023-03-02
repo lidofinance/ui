504247c7730f85a4d9bf0db8fe73fc6ee81ef8f6
Nikita Kozlov <nikita.kozlov@p2p.org>
2023-03-02 20:36:06 +0300

    feat: replace storybook cli option with config

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  staticDirs: ['./public'],
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
