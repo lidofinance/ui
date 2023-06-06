9d7842aaa72ff71f511d09f6e0529ec77df832b5
Nikita <nikita.k@lido.fi>
2023-06-06 20:19:14 +0200

    fix: storybook & tests type check issue

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -5,7 +5,7 @@ const main = async () => {
   await $`rm -rf ./dist`
   await $`swc ./packages --no-swcrc --config-file .swcrc.commonjs -d ./dist/cjs`
   await $`swc ./packages --no-swcrc --config-file .swcrc.esm -d ./dist/esm`
-  await $`tsc --emitDeclarationOnly --outDir ./dist/types`
+  await $`tsc -p tsconfig.build.json --emitDeclarationOnly --outDir ./dist/types`
 }
 
 void main()
