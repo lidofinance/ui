2049e875f35462601fe85b7cf4503a2413d0804e
Nikita <nikita.k@lido.fi>
2023-07-12 20:05:06 +0200

    chore: format fix

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
-    ['@babel/preset-react', {runtime: 'automatic'}],
-    '@babel/preset-typescript'
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
   ],
-};
-
+}
