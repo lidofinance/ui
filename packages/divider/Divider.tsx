ebb5a0dc89f11d0c110527d3fd852f15bd503a33
Nikita <nikita.k@lido.fi>
2023-07-20 19:00:40 +0200

    feat: moved divider to css modules

--- packages/divider/Divider.tsx
+++ packages/divider/Divider.tsx
@@ -1,11 +1,54 @@
-import React, { ForwardedRef, forwardRef } from 'react'
-import { DividerStyle } from './DividerStyles'
-import { DividerProps } from './types'
+import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from 'react'
+import cn from 'classnames'
+import styles from './Divider.module.css'
 
-function Divider(props: DividerProps, ref?: ForwardedRef<HTMLDivElement>) {
-  const { type = 'horizontal', indents, children, ...rest } = props
+export enum DividerType {
+  vertical,
+  horizontal,
+}
+export type DividerTypes = keyof typeof DividerType
 
-  return <DividerStyle $type={type} $indents={indents} ref={ref} {...rest} />
+export enum DividerIndent {
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+}
+export type DividerIndents = keyof typeof DividerIndent
+
+export type DividerProps = ComponentPropsWithoutRef<'div'> & {
+  type?: DividerTypes
+  indents?: DividerIndents
+  children?: never
 }
 
-export default forwardRef(Divider)
+export const Divider = forwardRef(
+  (
+    {
+      type = 'horizontal',
+      indents,
+      className,
+      children,
+      ...rest
+    }: DividerProps,
+    ref?: ForwardedRef<HTMLDivElement>,
+  ) => {
+    return (
+      <div
+        className={cn(styles.divider, className, {
+          [styles.xs]: indents === 'xs',
+          [styles.sm]: indents === 'sm',
+          [styles.md]: indents === 'md',
+          [styles.lg]: indents === 'lg',
+          [styles.xl]: indents === 'xl',
+          [styles.horizontal]: type === 'horizontal',
+          [styles.vertical]: type === 'vertical',
+        })}
+        ref={ref}
+        {...rest}
+      />
+    )
+  },
+)
+Divider.displayName = 'Divider'
--- packages/divider/index.ts
+++ packages/divider/index.ts
@@ -1,2 +1,1 @@
-export { default as Divider } from './Divider'
-export * from './types'
+export * from './Divider'
