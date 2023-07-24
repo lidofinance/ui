7644658ebb0ed01abcc2d06c3aea828b8d1016a4
Nikita <nikita.k@lido.fi>
2023-07-24 19:28:45 +0200

    feat: moved Link to css modules

--- packages/link/Link.tsx
+++ packages/link/Link.tsx
@@ -1,11 +1,27 @@
-import React, { ForwardedRef, forwardRef } from 'react'
-import { LinkStyle } from './LinkStyles'
-import { LinkProps } from './types'
+import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from 'react'
+import styles from './LinkStyles.module.css'
+import cn from 'classnames'
 
-function Link(props: LinkProps, ref?: ForwardedRef<HTMLAnchorElement>) {
-  return (
-    <LinkStyle target='_blank' rel='nofollow noopener' ref={ref} {...props} />
-  )
+export type LinkProps = ComponentPropsWithoutRef<'a'> & {
+  fadeVisited?: boolean
 }
 
-export default forwardRef(Link)
+export const Link = forwardRef(
+  (
+    { fadeVisited, className, children, ...rest }: LinkProps,
+    ref?: ForwardedRef<HTMLAnchorElement>,
+  ) => {
+    return (
+      <a
+        className={cn(styles.link, { [styles.fadedVisited]: fadeVisited })}
+        target='_blank'
+        rel='nofollow noopener'
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </a>
+    )
+  },
+)
+Link.displayName = 'Link'
