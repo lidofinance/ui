7d47e68b6bd2dd12ad795a437d33185fdb1dddc2
Nikita <nikita.k@lido.fi>
2023-08-01 16:53:42 +0200

    feat: font is custom uihead property

--- packages/theme/utils/document-head-contents.tsx
+++ packages/theme/utils/document-head-contents.tsx
@@ -67,8 +67,20 @@ export const initColors = () => {
   initGlobalCookieTheme()
 }
 
+const CSS_FONTS = `@import url(https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800);
+body {
+font-family: Manrope, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}`
+
+export const Fonts = (): JSX.Element => <style>{CSS_FONTS}</style>
+
 export const LidoUIHead: FC<{
   jsStyleScript?: boolean
-}> = ({ jsStyleScript = true }) => (
-  <>{jsStyleScript ? <ScriptThemeValue /> : null}</>
+  cssFont?: boolean
+}> = ({ jsStyleScript = true, cssFont = false }) => (
+  <>
+    {jsStyleScript ? <ScriptThemeValue /> : null}
+    {cssFont ? <Fonts /> : null}
+  </>
 )
