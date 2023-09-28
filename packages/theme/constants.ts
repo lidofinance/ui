0dfb477a60b689cc5cb155b0b4fe87a77644e0c1
Vsevolod Rodionov <Jabher@users.noreply.github.com>
2023-09-28 18:22:26 +0700

    fix: cookie theme is not assigning to specific domains threated as TLDs

--- packages/theme/constants.ts
+++ packages/theme/constants.ts
@@ -8,7 +8,7 @@ export const DEFAULT_THEME_NAME = ThemeName.light
 export const themeCookieKey = 'lido-theme-manual'
 export const globalStyleDataAttribute = 'data-lido-ui-global-style'
 
-export const themeCookieExpire = 365
+export const themeCookieMaxAge = 365 * 24 * 3600 // 365 days in seconds
 
 export const prefersDarkThemeMediaQuery =
   typeof window !== 'undefined'
