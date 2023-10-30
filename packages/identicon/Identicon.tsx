e01fd89c53d93efce298fa0476b635478a7e375a
Alexander Khramov <alx.khramov@gmail.com>
2023-10-30 19:22:51 +0300

    refactor: fix import from 'react-jazzicon'

--- packages/identicon/Identicon.tsx
+++ packages/identicon/Identicon.tsx
@@ -1,7 +1,9 @@
 import { ForwardedRef, forwardRef } from 'react'
 import { IdenticonProps } from './types.js'
 import { IdenticonStyle } from './IdenticonStyles.js'
-import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
+import Jazzicon from 'react-jazzicon'
+// There is an error with importing jsNumberForAddress as named export in ESM build
+import jsNumberForAddress from 'react-jazzicon/dist/jsNumberForAddress.js'
 
 export const Identicon = forwardRef(
   (
