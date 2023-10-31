1cb5a2fe20005611552bcf0081e25feb8cf78659
Alexander Khramov <alx.khramov@gmail.com>
2023-10-31 15:30:12 +0300

    chore: configure jest to work with ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@lidofinance/(.*)$': '<rootDir>/packages/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
 }
