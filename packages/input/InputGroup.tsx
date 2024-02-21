c90a1ff410b170f2c3439ff51e9bb2ffdabca054
Karina Maulitova <karinamaulitova@gmail.com>
2024-02-21 13:09:12 +0100

    feat: add warning props to inputGroup

--- packages/input/InputGroup.tsx
+++ packages/input/InputGroup.tsx
@@ -5,11 +5,19 @@ import { InputGroupProps } from './types.js'
 
 export const InputGroup = forwardRef(
   (
-    { fullwidth = false, error, success, children, ...rest }: InputGroupProps,
+    {
+      fullwidth = false,
+      error,
+      success,
+      warning,
+      children,
+      ...rest
+    }: InputGroupProps,
     ref?: ForwardedRef<HTMLSpanElement>,
   ) => {
     const hasError = !!error
     const hasSuccess = !!success && !error
+    const hasWarning = !hasError && !!warning // `error` overrides `warning`
 
     return (
       <InputGroupStyle $fullwidth={fullwidth} {...rest} ref={ref}>
@@ -19,6 +27,9 @@ export const InputGroup = forwardRef(
         )}
         {hasSuccess && (
           <InputMessageStyle $variant='success'>{success}</InputMessageStyle>
+        )}
+        {hasWarning && (
+          <InputMessageStyle $variant='warning'>{warning}</InputMessageStyle>
         )}
       </InputGroupStyle>
     )
