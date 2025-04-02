224acc6c420db815c3fc7b84e48def5dfa5498ab
cakeinpanic <cakeinpanic@gmail.com>
2025-04-02 19:04:55 +0300

    chore: update default viewports in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,8 +4,44 @@ import './styles/storybook.css'
 import '../styles/global.css'
 import { Decorator } from '@storybook/react'
 
+const defaultViewports = {
+  px1500: {
+    name: '1500px',
+    styles: {
+      width: '1500px',
+      height: '500px',
+    },
+  },
+  px900: {
+    name: '900px',
+    styles: {
+      width: '900px',
+      height: '800px',
+    },
+  },
+  px600: {
+    name: '600px',
+    styles: {
+      width: '600px',
+      height: '800px',
+    },
+  },
+  px500: {
+    name: '500px',
+    styles: {
+      width: '500px',
+      height: '800px',
+    },
+  },
+}
+
 export const parameters = {
   controls: { hideNoControlsWarning: true },
+  viewport: {
+    viewports: {
+      ...defaultViewports,
+    },
+  },
 }
 export const args = {
   themeOverride: 'light',
