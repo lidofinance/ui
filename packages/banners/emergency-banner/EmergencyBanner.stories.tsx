21b359ece243cba23b704369cedf274a3f4f6650
cakeinpanic <cakeinpanic@gmail.com>
2025-04-09 12:39:51 +0300

    fix: security check

--- packages/banners/emergency-banner/EmergencyBanner.stories.tsx
+++ packages/banners/emergency-banner/EmergencyBanner.stories.tsx
@@ -5,7 +5,7 @@ export default {
   component: EmergencyBanner,
   title: 'Banners/EmergencyBanner',
   args: {
-    description: 'Keep calm and carry on',
+    description: 'Test text',
     title: 'Important announcement',
     link: 'https://example.com',
     linkText: 'Check out important details here',
