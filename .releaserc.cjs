034f98f170be009b9b0ccf3b0c1d65e48fc9a371
cakeinpanic <cakeinpanic@gmail.com>
2025-04-28 12:40:18 +0300

    fix: testing github releases

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -3,12 +3,22 @@ module.exports = {
     { name: 'lido-ui-v4' },
     {
       name: 'lido-ui-v4-canary', "channel": "alpha", "prerelease": "alpha"
+    },{
+      name: 'feature/we-1997-add-possibility-to-publish-releases-with-release-notes-for', "channel": "alpha", "prerelease": "test"
     },
   ],
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
-    '@semrel-extra/npm',
+    //'@semrel-extra/npm',
+    [
+      "@semantic-release/github",
+      {
+        "assets": [
+          { "path": "dist", "label": "Built library" }
+        ]
+      }
+    ]
   ],
   verifyConditions: [],
 }
