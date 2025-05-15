08ce363d46d16c4a470022cfde415440fe5a4da9
cakeinpanic <cakeinpanic@gmail.com>
2025-05-15 12:56:12 +0300

    fix: add showcase of all theme providers

--- packages/theme-css/theme-provider/theme-provider.stories.tsx
+++ packages/theme-css/theme-provider/theme-provider.stories.tsx
@@ -1,6 +1,7 @@
-import { StoryFn, Meta } from '@storybook/react'
-import { DarkThemeProvider, LightThemeProvider } from '.'
+import { Meta, StoryFn } from '@storybook/react'
+import { DarkThemeProvider, LightThemeProvider, ThemeProvider } from '.'
 import { Block } from '../block'
+import { ThemeName } from '../constants'
 
 export default {
   title: 'Theme/Providers',
@@ -9,12 +10,26 @@ export default {
 export const Providers: StoryFn = (props) => (
   <>
     <Block>Component inherits global theme</Block>
+    <p style={{ borderBottom: '1px solid black' }}></p>
+    <ThemeProvider themeName={ThemeName.light}>
+      <Block>
+        Wrapped in ThemeProvider with prop <i>light</i>
+      </Block>
+    </ThemeProvider>
+    <ThemeProvider themeName={ThemeName.dark}>
+      <Block>
+        Wrapped in ThemeProvider with prop <i>dark</i>
+      </Block>
+    </ThemeProvider>
+
+    <p style={{ marginTop: '20px', borderBottom: '1px solid black' }}></p>
     <LightThemeProvider>
       <Block>Wrapped in LightThemeProvider</Block>
     </LightThemeProvider>
     <DarkThemeProvider>
       <Block>Wrapped in DarkThemeProvider</Block>
     </DarkThemeProvider>
+    <p style={{ marginTop: '20px', borderBottom: '1px solid black' }}></p>
     <div
       style={
         {
--- packages/utils/get-top-level-domain.ts
+++ packages/utils/get-top-level-domain.ts
@@ -1,11 +1,12 @@
-export const getTopLevelDomain =
-  typeof window === 'undefined'
-    ? () => 'localhost'
-    : () => {
-        if (document.location.host.indexOf('localhost') === 0) {
-          return 'localhost'
-        } else {
-          // Get host with dot in first position
-          return `.${location.hostname.split('.').slice(-2).join('.')}`
-        }
-      }
+export const getTopLevelDomain = () => {
+  if (typeof window === 'undefined') {
+    return 'localhost'
+  }
+  const pathParts = window.location.hostname.split('.')
+
+  if (pathParts[pathParts.length - 1] === 'localhost') {
+    return 'localhost'
+  }
+  // Get host with dot in first position
+  return `.${pathParts.slice(-2).join('.')}`
+}
