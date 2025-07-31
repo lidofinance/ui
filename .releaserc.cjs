818542e3da29217a5b80d69eaebd6e1e439dc494
cakeinpanic <cakeinpanic@gmail.com>
2025-07-31 11:08:46 +0300

    feat: update npm package dependencies and replace @semrel-extra/npm with @semantic-release/npm

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
-    '@semrel-extra/npm',
+    "@semantic-release/npm",
     [
       "@semantic-release/github"
     ]
