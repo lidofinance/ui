82444a9ee7985691c02c3190352f633f853476d9
Alex Mantsurov <molok0aleks99@gmail.com>
2025-08-11 12:12:09 +0300

    feat: added more icons and logos

---
+++ packages/icons/components/general/ImmunefiLight.tsx
@@ -0,0 +1,48 @@
+// THIS FILE IS AUTO GENERATED
+
+import React, { SVGProps } from 'react'
+
+export const ImmunefiLight = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width={80} height={80} viewBox='0 0 80 80' fill='none' {...props}>
+      <g
+        opacity={0.4}
+        filter='url(#ImmunefiLight__ImmunefiLight__filter0_f_1533_25801)'
+      >
+        <path
+          d='M33.08 23.4L22.9 33.595l4.8 4.798 12.102-12.117v-5.933h4.71v6.337l11.86 11.87 4.737-4.744-10.341-10.344 3.495-3.4L68 33.813l-8.288 8.292 8.297 8.308-13.585 13.595-3.495-3.402 10.18-10.193-4.8-4.798-11.797 11.81v6.24h-4.71v-6.034L27.637 45.449 22.9 50.194l10.341 10.344-3.495 3.4L16 50.194l8.295-8.3-8.295-8.3L29.585 20l3.495 3.4zm-2.04 18.556L42.053 52.98l10.919-10.924-11.018-11.028L31.04 41.956z'
+          fill='#fff'
+        />
+      </g>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M31.08 21.4L20.9 31.595l4.8 4.798 12.1-12.116v-5.934h4.71v6.336L54.373 36.55l4.737-4.745-10.341-10.344 3.495-3.4L66 31.813l-8.288 8.292 8.297 8.308L52.424 62.01l-3.495-3.402 10.18-10.193-4.8-4.798-11.798 11.812v6.238H37.8v-6.035L25.637 43.449 20.9 48.194l10.341 10.344-3.495 3.4L14 48.194l8.295-8.3-8.295-8.3L27.585 18l3.495 3.4zm-2.04 18.556L40.053 50.98l10.919-10.924-11.018-11.028L29.04 39.956z'
+        fill='#fff'
+      />
+      <defs>
+        <filter
+          id='ImmunefiLight__ImmunefiLight__filter0_f_1533_25801'
+          x={8}
+          y={12}
+          width={68.009}
+          height={60.009}
+          filterUnits='userSpaceOnUse'
+          colorInterpolationFilters='sRGB'
+        >
+          <feFlood floodOpacity={0} result='BackgroundImageFix' />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='BackgroundImageFix'
+            result='shape'
+          />
+          <feGaussianBlur
+            stdDeviation={4}
+            result='effect1_foregroundBlur_1533_25801'
+          />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
