f2f53d4e7ca2a67dc4f4fd40937cbf55b931d123
Alex Mantsurov <molok0aleks99@gmail.com>
2025-08-21 13:17:32 +0300

    feat: added MenuInstiLight icon

---
+++ packages/icons/components/general/MenuInstiLight.tsx
@@ -0,0 +1,88 @@
+// THIS FILE IS AUTO GENERATED
+
+import React, { SVGProps } from 'react'
+
+export const MenuInstiLight = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width={80} height={80} viewBox='0 0 80 80' fill='none' {...props}>
+      <g
+        opacity={0.5}
+        filter='url(#MenuInstiLight__MenuInstiLight__filter0_f_1690_18865)'
+        fill='#fff'
+      >
+        <path
+          opacity={0.6}
+          d='M53.767 39.471l.403.614c4.546 6.92 3.53 15.984-2.44 21.79C48.215 65.291 43.611 67 39.007 67l14.76-27.529z'
+        />
+        <path opacity={0.2} d='M39.006 47.838l14.76-8.367L39.006 67V47.838z' />
+        <path d='M24.233 39.471l-.403.614c-4.546 6.92-3.53 15.984 2.44 21.79C29.785 65.291 34.389 67 38.993 67L24.232 39.47z' />
+        <path opacity={0.6} d='M38.988 47.838l-14.76-8.367L38.989 67V47.838z' />
+        <path opacity={0.2} d='M39.012 29.147v14.43l12.713-7.21-12.713-7.22z' />
+        <path
+          opacity={0.6}
+          d='M39.008 29.147l-12.723 7.22 12.723 7.21v-14.43z'
+        />
+        <path d='M39.008 17.012l-12.723 19.36 12.723-7.24v-12.12z' />
+        <path opacity={0.6} d='M39.012 29.13l12.723 7.24L39.012 17v12.13z' />
+      </g>
+      <path
+        opacity={0.6}
+        d='M54.767 36.471l.403.614c4.546 6.92 3.53 15.984-2.44 21.79C49.215 62.291 44.611 64 40.007 64l14.76-27.529z'
+        fill='#fff'
+      />
+      <path
+        opacity={0.2}
+        d='M40.006 44.838l14.76-8.367L40.006 64V44.838z'
+        fill='#fff'
+      />
+      <path
+        d='M25.233 36.471l-.403.614c-4.546 6.92-3.53 15.984 2.44 21.79C30.785 62.291 35.389 64 39.993 64L25.232 36.47z'
+        fill='#fff'
+      />
+      <path
+        opacity={0.6}
+        d='M39.988 44.838l-14.76-8.367L39.989 64V44.838z'
+        fill='#fff'
+      />
+      <path
+        opacity={0.2}
+        d='M40.012 26.147v14.43l12.713-7.21-12.713-7.22z'
+        fill='#fff'
+      />
+      <path
+        opacity={0.6}
+        d='M40.008 26.147l-12.723 7.22 12.723 7.21v-14.43z'
+        fill='#fff'
+      />
+      <path d='M40.008 14.011l-12.723 19.36 12.723-7.24v-12.12z' fill='#fff' />
+      <path
+        opacity={0.6}
+        d='M40.012 26.13l12.723 7.24L40.012 14v12.13z'
+        fill='#fff'
+      />
+      <defs>
+        <filter
+          id='MenuInstiLight__MenuInstiLight__filter0_f_1690_18865'
+          x={13}
+          y={9}
+          width={52}
+          height={66}
+          filterUnits='userSpaceOnUse'
+          colorInterpolationFilters='sRGB'
+        >
+          <feFlood floodOpacity={0} result='BackgroundImageFix' />
+          <feBlend
+            mode='normal'
+            in='SourceGraphic'
+            in2='BackgroundImageFix'
+            result='shape'
+          />
+          <feGaussianBlur
+            stdDeviation={4}
+            result='effect1_foregroundBlur_1690_18865'
+          />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
