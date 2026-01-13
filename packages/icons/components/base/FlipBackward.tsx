24c8f062211ff959ec1f11f7d196c5961300acbf
cakeinpanic <cakeinpanic@gmail.com>
2026-01-13 18:35:24 +0200

    fix(icons): update gradientTransform for Lido Ecosystem icon

---
+++ packages/icons/components/base/FlipBackward.tsx
@@ -0,0 +1,15 @@
+// THIS FILE IS AUTO GENERATED
+
+import React, { SVGProps } from 'react'
+
+export const FlipBackward = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width={24} height={24} viewBox='0 0 24 24' fill='none' {...props}>
+      <path
+        d='M3 9h13.5a4.5 4.5 0 110 9H12M3 9l4-4M3 9l4 4'
+        stroke='currentColor'
+        strokeWidth={1.4}
+      />
+    </svg>
+  )
+}
---
+++ packages/icons/components/base/Including.tsx
@@ -0,0 +1,14 @@
+// THIS FILE IS AUTO GENERATED
+
+import React, { SVGProps } from 'react'
+
+export const Including = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg width={24} height={24} viewBox='0 0 24 24' fill='none' {...props}>
+      <path
+        d='M18.81 13.894l-.495.494-3.354 3.355-.99-.99 2.16-2.16H8.89a3.7 3.7 0 01-3.7-3.7V6.258h1.4v4.637a2.3 2.3 0 002.3 2.3h7.242l-2.161-2.16.99-.991 3.85 3.85z'
+        fill='currentColor'
+      />
+    </svg>
+  )
+}
