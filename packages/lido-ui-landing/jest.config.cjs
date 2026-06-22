0e245b74c5719c621db2101c5e683c648dcd2eef
cakeinpanic <cakeinpanic@gmail.com>
2026-06-22 11:47:05 +0300

    fix: update Jest configuration to ignore

--- packages/lido-ui-landing/jest.config.cjs
+++ packages/lido-ui-landing/jest.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleNameMapper: {
     '^@lidofinance/(.*)$': '<rootDir>/src/$1',
     '^(\\.{1,2}/.*)\\.js$': '$1',
